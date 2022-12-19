2d5fe38cc3b56b3f885fdb4f5db085e15f23c63b
Kuzminih305 <Kuzminih187@gmail.com>
2022-12-19 20:27:32 +0300

    beast

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/accordion/Accordion";
+import {Rating} from "./components/rating/Rating";
+
+function Hello() {
+    alert("Hello It-Kamasutra")
+}
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    console.log("App rendering")
+    return (
+        <div>
+            <PageTitle title={"This is App component"}/>
+            <PageTitle title={"My Friends"}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion titleValue={"Меню"} collapsed={false}/>
+            <Accordion titleValue={"Цена"} collapsed={false}/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
+}
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    console.log("PageTitle rendering")
+    return <h1>{props.title}</h1>
 }
 
 export default App;
